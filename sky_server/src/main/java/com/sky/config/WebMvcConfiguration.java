d8e658dca716d801af5ca3aa06e7a2c8b9f1f88d
Jeason <19938943480@163.com>
2024-01-09 16:26:52 +0800

    修复knife4j文档请求异常问题

--- sky_server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky_server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -2,12 +2,15 @@ package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
 import com.sky.json.JacksonObjectMapper;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -15,6 +18,7 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.HashMap;
 import java.util.List;
@@ -22,14 +26,12 @@ import java.util.Map;
 
 /**
  * 配置类，注册web层相关组件
- * 打开api文档的地质: http:localhost:8080/doc.html
- * 其中localhost代表ip，8080代表端口号，可以更换
+ * api文档的地址: http:ip:port/doc.html
+ * 其中ip为localhost，port为8080，可以更换
  */
 @Configuration
 @Slf4j
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
-
-
     @Autowired
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
 
@@ -42,7 +44,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
-                .excludePathPatterns("/admin/employee/login","/doc.html");
+                .excludePathPatterns("/admin/employee/login");
     }
 
     /**
@@ -50,8 +52,12 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * @param registry
      */
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/doc.html").
+                addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").
+                addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
     }
 
     /**
@@ -59,38 +65,42 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      *
      * @return the global open api customizer
      */
-//    @Bean
-//    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
-//        return openApi -> {
-//            if (openApi.getTags()!=null){
-//                openApi.getTags().forEach(tag -> {
-//                    Map<String,Object> map=new HashMap<>();
-//                    map.put("x-order", 1);
-//                    tag.setExtensions(map);
-//                });
-//            }
-//            if(openApi.getPaths()!=null){
-//                openApi.addExtension("x-test123","333");
-//                openApi.getPaths().addExtension("x-abb",1);
-//            }
-//
-//        };
-//    }
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .title("苍穹外卖接口文档")
-//                        .version("1.0")
-//                        .description( "苍穹外卖接口文档")
-//                        .termsOfService("http://doc.xiaominfo.com")
-//                        .license(new License().name("Apache 2.0")
-//                                .url("http://doc.xiaominfo.com")));
-//    }
+    @Bean
+    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
+        return openApi -> {
+            if (openApi.getTags()!=null){
+                openApi.getTags().forEach(tag -> {
+                    Map<String,Object> map=new HashMap<>();
+                    map.put("x-order", 1);
+                    tag.setExtensions(map);
+                });
+            }
+            if(openApi.getPaths()!=null){
+                openApi.addExtension("x-test123","333");
+                openApi.getPaths().addExtension("x-abb",1);
+            }
+
+        };
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("苍穹外卖接口文档")
+                        // 接口文档简介
+                        .description("这是基于Knife4j OpenApi3的接口文档")
+                        // 接口文档版本
+                        .version("v1.0")
+                        // 开发者联系方式
+                        .contact(new Contact().name("JasonSteve").email("19938943480@163.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringBoot基础框架")
+                        .url("http://127.0.0.1:8080"));
+    }
 
     /**
      * 扩展消息转换器,将日期类型从列表转换为时间戳
+     * 这个是导致knife4j不能正常显示的罪魁祸首,特别要注意添加的位置
      * @param converters 消息转换器列表
      */
     @Override
@@ -98,7 +108,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
         jackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
 
-        converters.add(0,jackson2HttpMessageConverter);
-
+//        converters.add(0,jackson2HttpMessageConverter);
+        converters.add(converters.size()-1,jackson2HttpMessageConverter);
     }
 }
